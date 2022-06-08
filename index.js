8b4e95f66452b4879caa28ad859f2a4018b26dd6
DENNISINNOCENT <mutindahdennis@gmail.com>
2022-06-08 21:45:41 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+function saturdayFun(activity="roller-skate"){
+  return `This Saturday, I want to ${activity}!`
+}
+activity()
+ 
+
+// let mondayWork = function(activity= `go to the office`){
+//   return `This Monday, I will work ${activity}.`
+// }
+function mondayWork(activity = 'go to the office'){
+   return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective (or){
+  return function(value="special"){
+   return `You are ${or}${value}${or}!`
+ } 
+
+}
+wrapAdjective("%")("a dedicated programmer");
